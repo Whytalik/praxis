5c74c7e36d9638c3f751ad4d1b5e6b99893ad988
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2025-05-09 21:56:46 +0300

    feat: alias and gitignore configurated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@layout": path.resolve(__dirname, "./src/layout"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@views": path.resolve(__dirname, "./src/views"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
